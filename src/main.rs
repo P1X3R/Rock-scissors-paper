a571db6f9d63c7af4842a29e0590fe5e43bc8996
P1x3r <justthepixuser@hotmail.com>
2024-04-23 23:04:28 +0000

    Can get input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io::stdin;
+
+const OPTIONS: (&str, &str, &str) = ("rock", "scissors", "paper");
+
+fn get_input(msg: &str) {
+    let mut input = String::new();
+    println!("{msg}");
+    stdin().read_line(&mut input).unwrap();
+
+    match input.trim() {
+        "rock" => print!(""),
+        "scissors" => print!(""),
+        "paper" => print!(""),
+        "q" => std::process::exit(400),
+        _ => println!("There's no a valid option")
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        get_input("Select your option (rock, scissors or paper): ");
+    }
 }
